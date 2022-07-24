b08f62a520bd19ee87690a1161e3901adb46d5c8
r.allahyari <r.allahyari@R-ALLAHYARI>
2022-07-24 13:49:29 +0430

    add some specflow scenarios

---
+++ Mc2.CrudTest.AcceptanceTests/CustomerCreationFeatureSteps.cs
@@ -0,0 +1,9 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Mc2.CrudTest.AcceptanceTests
+{
+    [Binding]
+    public class CustomerCreationFeatureSteps
+    {
+    }
+}
---
+++ Mc2.CrudTest.AcceptanceTests/SpecCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mc2.CrudTest.AcceptanceTests
+{
+    public class SpecCustomer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string BankAccountNumber { get; set; }
+    }
+}

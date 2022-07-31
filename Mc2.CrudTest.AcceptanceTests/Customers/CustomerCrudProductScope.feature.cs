4a7a4d029b2fc8a114dcd38c3308cdc7d39d4a1f
r.allahyari <r.allahyari@R-ALLAHYARI>
2022-07-31 10:23:18 +0430

    add blazor pages

--- Mc2.CrudTest.AcceptanceTests/Customers/CustomerCrudProductScope.feature.cs
+++ Mc2.CrudTest.AcceptanceTests/Customers/CustomerCrudProductScope.feature.cs
@@ -40,7 +40,14 @@ namespace Mc2.CrudTest.AcceptanceTests.Customers
         public static void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Customers", "Customer Create Read Edit Delete Product Scope", null, ProgrammingLanguage.CSharp, ((string[])(null)));
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Customers", "Customer Create Read Edit Delete Product Scope", @"    Background
+        Given system error codes are following
+          | Code | Description                                                |
+          | 101  | Invalid Mobile Number                                      |
+          | 102  | Invalid Email address                                      |
+          | 103  | Invalid Bank Account Number                                |
+          | 201  | Duplicate customer by First-name, Last-name, Date-of-Birth |
+          | 202  | Duplicate customer by Email address                        |", ProgrammingLanguage.CSharp, ((string[])(null)));
             testRunner.OnFeatureStart(featureInfo);
         }
         
@@ -78,6 +85,79 @@ namespace Mc2.CrudTest.AcceptanceTests.Customers
         void System.IDisposable.Dispose()
         {
             this.TestTearDown();
+        }
+        
+        [Xunit.SkippableTheoryAttribute(DisplayName="Create Read Edit Delete Customer")]
+        [Xunit.TraitAttribute("FeatureTitle", "Customer Create Read Edit Delete Product Scope")]
+        [Xunit.TraitAttribute("Description", "Create Read Edit Delete Customer")]
+        [Xunit.InlineDataAttribute("1", "John", "Doe", "john@doe.com", "+989121234567", "01-JAN-2000", "IR000000000000001", new string[0])]
+        public virtual void CreateReadEditDeleteCustomer(string iD, string firstName, string lastName, string email, string phoneNumber, string dateOfBirth, string bankAccountNumber, string[] exampleTags)
+        {
+            string[] tagsOfScenario = exampleTags;
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            argumentsOfScenario.Add("ID", iD);
+            argumentsOfScenario.Add("FirstName", firstName);
+            argumentsOfScenario.Add("LastName", lastName);
+            argumentsOfScenario.Add("Email", email);
+            argumentsOfScenario.Add("PhoneNumber", phoneNumber);
+            argumentsOfScenario.Add("DateOfBirth", dateOfBirth);
+            argumentsOfScenario.Add("BankAccountNumber", bankAccountNumber);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Create Read Edit Delete Customer", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 12
+    this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 13
+        testRunner.When(string.Format("user creates customer with {0}", firstName), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 14
+        testRunner.And(string.Format("lastname of {0}", lastName), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 15
+        testRunner.And(string.Format("date of birth of {0}", dateOfBirth), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 16
+        testRunner.And(string.Format("email of {0}", email), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 17
+        testRunner.And(string.Format("Phone number of {0}", phoneNumber), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 18
+        testRunner.Then(string.Format("user can lookup customer by ID of {0} and get \"1\" records", iD), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+#line 19
+        testRunner.When(string.Format("user edit customer by ID of {0} with new email of \"new@email.com\"", iD), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 20
+        testRunner.Then(string.Format("user can lookup customer by ID of {0} and get \"1\" records", iD), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+#line 21
+        testRunner.And("return record email is \"new@email.com\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 22
+        testRunner.When(string.Format("user delete customer by ID of {0}", iD), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 23
+        testRunner.Then(string.Format("user can lookup customer by ID of {0} and get \"0\" records", iD), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
         }
         
         [Xunit.SkippableTheoryAttribute(DisplayName="Create a duplicate customer by firstname, lastname and date of birth")]
---
+++ Mc2.CrudTest.Presentation.Contract/CreateCustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mc2.CrudTest.Presentation.Contract
+{
+    public class CreateCustomerModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string BankAccountNumber { get; set; }
+    }
+}
